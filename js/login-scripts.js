b1a8f0c723075278a3efbd36f981fb25cb045f6b
Tamires Calixto <107005280+tamirescalixto@users.noreply.github.com>
2022-08-23 14:34:35 -0300

    Update login-scripts.js

--- js/login-scripts.js
+++ js/login-scripts.js
@@ -6,3 +6,25 @@ hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     menuNav.classList.toggle('active');
 })
+
+// Login form
+function logar() {
+
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+    if (document.getElementById("email").value == "") {
+        alert('Por favor, preencha o campo email!');
+        document.getElementById("email").focus();
+        return false
+    } 
+
+    if (document.getElementById("senha").value == "") {
+        alert('Por favor, preencha o campo senha!');
+        document.getElementById("senha").focus();
+        return false;
+    } else {
+        location.href = "index.html";
+    }
+    
+}
